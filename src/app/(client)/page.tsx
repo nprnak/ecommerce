d194599c4b475b4eae57a642248f17c7eac3425d
pranaya <pranayanakarmi@gmail.com>
2025-07-11 12:13:25 +0545

    category in homepage

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,9 +1,11 @@
+import BrowseCategories from "@/components/client/browsecategory";
 import FlashSalesCarousel from "@/components/client/flashsales";
 import React from "react";
 export default function Home() {
   return (
     <>
       <FlashSalesCarousel />
+      <BrowseCategories />
     </>
   );
 }
